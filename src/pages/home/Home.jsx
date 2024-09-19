316d8e140a30007e8a223ef85807260fca7f6279
shaebukhan <97784597+shaebukhan@users.noreply.github.com>
2024-09-19 18:12:55 +0500

    Add files via upload

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import HomeSec from "../../components/HomeSec";
+
+
+
+const Home = (props) => {
+    return (
+        <>
+            <Navbar />
+            <HomeSec />
+            {/* <Footer /> */}
+        </>
+    );
+};
+
+export default Home;
